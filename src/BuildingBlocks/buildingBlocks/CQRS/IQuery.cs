ad16e9619f5f0db085453945ae2cc9479f0e75b1
3morii74 <omerahmed200237@gmail.com>
2025-02-18 13:37:38 +0200

    CQRS Interface

---
+++ src/BuildingBlocks/buildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace buildingBlocks.CQRS
+{
+    public interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+    {
+    }
+}
---
+++ src/BuildingBlocks/buildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace buildingBlocks.CQRS
+{
+    internal interface IQueryHandler
+    {
+        public interface IQueryHandler<in TQuery, TResponse>
+        : IRequestHandler<TQuery, TResponse>
+        where TQuery : IQuery<TResponse>
+        where TResponse : notnull
+        {
+        }
+    }
+}
